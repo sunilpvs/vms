bcc86d24f02c15ac9fc8dd9eafeb0cb92ec7e42c
Haritha reddy <haritha.p@pvs-consultancy.com>
2025-12-31 17:40:53 +0530

    message

--- src/pages/vms/VendorListTable.jsx
+++ src/pages/vms/VendorListTable.jsx
@@ -319,9 +319,9 @@ const filteredVendors = Array.isArray(vendors)
         </DialogTitle>
 
         <DialogContent>
-          <Typography sx={{ color: "#000", mb: 1, fontSize: "16px" }}>
-            Are you sure you want to re-initiate vendor: {" "}
-            <strong>{selectedVendor?.vendor_code}</strong>?
+          <Typography sx={{ color: "#000", mb: 1 }}>
+            Are you sure you want to re-initiate{" "}
+            <strong>{selectedVendor?.entity_name}</strong>?
           </Typography>
 
           {/* Warning text */}
